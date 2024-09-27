8604c8da1012fca4bb2f881692ecee6852330c4c
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2024-09-27 22:23:35 +0330

    feat: install Primevue UI component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  modules: [
+    '@primevue/nuxt-module'
+],
+primevue: {
+  options: {
+      theme: {
+          preset: Aura
+      }
+  }
+},
   css: ['~/assets/css/tailwind.css'],
   postcss: {
     plugins: {
