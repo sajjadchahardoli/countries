d50e4d34edfb54f69191aca8e930e1b0e0bdc37a
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2024-10-10 22:49:47 +0330

    feat: add custom colors to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,16 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'midnight-blue': 'hsl(209, 23%, 22%)',   // Dark Blue for dark mode elements
+        'deep-navy': 'hsl(207, 26%, 17%)',       // Very Dark Blue for dark mode background
+        'charcoal': 'hsl(200, 15%, 8%)',         // Very Dark Blue for light mode text
+        'stone-gray': 'hsl(0, 0%, 52%)',         // Dark Gray for light mode input
+        'mist-gray': 'hsl(0, 0%, 98%)',          // Very Light Gray for light mode background
+        'white': 'hsl(0, 0%, 100%)',             // White for text in dark mode
+      } 
+    },
   },
   plugins: [],
 }
