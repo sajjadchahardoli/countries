178b7e52b2c8831f7600109f764103cbc29ccc9d
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2024-10-11 16:56:15 +0330

    add theme store to manage dark and light mode

---
+++ stores/theme.js
@@ -0,0 +1,22 @@
+// stores/theme.js
+import { defineStore } from 'pinia'
+
+export const useThemeStore = defineStore('theme', {
+  state: () => ({
+    isDark: false,
+  }),
+  actions: {
+    toggleThemeMode() {
+      this.isDark = !this.isDark
+      document.documentElement.classList.toggle('dark', this.isDark)
+      localStorage.setItem('theme', this.isDark ? 'dark' : 'light')
+    },
+    loadTheme() {
+      const savedTheme = localStorage.getItem('theme')
+      if (savedTheme) {
+        this.isDark = savedTheme === 'dark'
+        document.documentElement.classList.toggle('dark', this.isDark)
+      }
+    },
+  },
+})
