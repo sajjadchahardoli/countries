28a9bc495d9b4ba4bb3d8722f80ba7e9b10153d7
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2024-10-11 16:53:18 +0330

    add sun icon to Font Awesom library

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,8 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMoon, faSearch, faChevronDown, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faMoon, faSun, faSearch, faChevronDown, faArrowLeft, } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faMoon, faSearch, faChevronDown, faArrowLeft)
+library.add(faMoon, faSun, faSearch, faChevronDown, faArrowLeft)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
