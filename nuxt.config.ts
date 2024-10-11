97bab0e17431be5957bd2a39ecdff46ce13950d8
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2024-10-11 16:45:50 +0330

    chore: configure PrimeVue to use unstyled components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import Aura from '@primevue/themes/aura';
+import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: [
-    '@primevue/nuxt-module',
-    '@pinia/nuxt',
-    '@nuxtjs/google-fonts'
-],
-primevue: {
-  options: {
-      theme: {
-          preset: Aura
-      }
-  }
-},
-  css: ['~/assets/css/tailwind.css'],
+  modules: ["@primevue/nuxt-module", "@pinia/nuxt", "@nuxtjs/google-fonts"],
+  primevue: {
+    options: {
+      ripple: true,
+      unstyled: true,
+    },
+    importPT: { as: "Aura", from: "~/assets/theme" },
+    directives: {
+      include: ["Ripple"],
+    },
+  },
+  css: ["~/assets/css/tailwind.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  plugins: ['~/plugins/fontawesome.js'],
+  plugins: ["~/plugins/fontawesome.js"],
   build: {
-    transpile: ['@fortawesome/vue-fontawesome'],
+    transpile: ["@fortawesome/vue-fontawesome"],
   },
   googleFonts: {
     families: {
-      'Nunito+Sans': [300, 600, 800], 
+      "Nunito+Sans": [300, 600, 800],
     },
-    display: 'swap', 
+    display: "swap",
   },
-})
+});
